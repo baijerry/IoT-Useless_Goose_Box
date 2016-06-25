8e647b0d33b21b4fe6bf918aadfeedf6cd253071
Jerry Bai <jerrybai1@hotmail.com>
2016-06-25 15:53:32 -0400

    first commit

---
+++ actions.h
@@ -0,0 +1,26 @@
+#ifndef actions_h
+#define actions_h
+
+/* Action Class
+ * Holds all the possible actions that can be performed by the box
+ */
+
+class Action {
+  private:
+
+  public:
+    //Define static action functions here
+
+    //ToDo: Lid Movement functions (normal, fast, slow, shake)
+
+    //ToDo: Lid LED functions (on, delayed on, off, flicker)
+
+    //ToDo: Arm Movement (normal, fast, slow, shake)
+
+    //ToDo: Goose Red LED functions (on, delayed on, off, flicker)
+
+    //ToDo: Define preset functions 
+
+};
+
+#endif
